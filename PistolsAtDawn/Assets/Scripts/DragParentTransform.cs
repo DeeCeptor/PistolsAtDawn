6b7bdcb57b9d782ccecbb0a3d4961d5457b3ce41
Michael Long <sirmichaellong@gmail.com>
2014-11-16 17:29:42 -0600

    Worked on object rotation. Added matchlock and key. THE FRIZZEN ROTATES! Watch out for the ramrod.

--- PistolsAtDawn/Assets/Scripts/DragParentTransform.cs
+++ PistolsAtDawn/Assets/Scripts/DragParentTransform.cs
@@ -10,7 +10,7 @@ using System.Collections;
  * 
  * http://en.wikipedia.org/wiki/Musket#Operation
  */ 
-public class DraggableObject : MonoBehaviour 
+public class DragParentTransform : MonoBehaviour 
 {
 	public bool draggable = true;		// Is the object currently draggable?
 	public bool rotatable = true;
@@ -18,23 +18,22 @@ public class DraggableObject : MonoBehaviour
 	public Vector3 scrollDirection2 = Vector3.forward;
 
 	private bool dragging = false;
-
-	private Rigidbody rigidbody;
-
+	
+	private GameObject parent;
+	
 	private Vector3 screenPoint;
 	private Vector3 offset;
-
+	
 	private float interactionPlaneOffset = 3;	// The distance in FRONT of the camera that the interaction plane
-												// is located. The plane is viewed head-on from the camera.
-
-
+	// is located. The plane is viewed head-on from the camera.
+	
+	
 	void Start()
 	{
-		rigidbody = this.GetComponent<Rigidbody>();
-		rigidbody.velocity = Vector3.zero;
+		parent = this.transform.parent.gameObject;
 	}
-
-
+	
+	
 	/**
 	 * Called when left mouse is clicked while it's over this collider.
 	 * Reset variables for dragging around screen
@@ -43,31 +42,24 @@ public class DraggableObject : MonoBehaviour
 	{
 		if(draggable)
 		{
-			// Completely stop object from moving after it's selected
-			rigidbody.useGravity = false;
-			rigidbody.angularVelocity = Vector3.zero;
-			rigidbody.velocity = Vector3.zero;
-
-			rigidbody.constraints = RigidbodyConstraints.FreezeRotation;
-
 			// Set the Z of the screenPoint to be the plane with which the gun is situated
 			screenPoint = Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x,
 			                                                         gameObject.transform.position.y,
-				                                                     interactionPlaneOffset));
-
+			                                                         interactionPlaneOffset));
+			
 			// Offset of center of object to where the mouse cursor is when it's first clicked
 			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
-
+			
 			// Reset rotation of object
-			transform.rotation = Quaternion.identity;
-
+			//transform.rotation = Quaternion.identity;
+			
 			v3StartPos = Input.mousePosition;
 			v3StartRot = transform.eulerAngles;
 			v3ChangedPos = v3StartPos;
 		}
 	}
-
-
+	
+	
 	/**
 	 * Called when the mouse is released ovr this collider.
 	 */ 
@@ -75,15 +67,10 @@ public class DraggableObject : MonoBehaviour
 	{
 		if(draggable)
 		{
-			dragging = false;
 
-			rigidbody.constraints = RigidbodyConstraints.None;
-			rigidbody.useGravity = true;
-			rigidbody.angularVelocity = Vector3.zero;
-			rigidbody.velocity = Vector3.zero;
 		}
 	}
-
+	
 	private float horizontalRotationalFactor = 3f;
 	private float verticalRotationalFactor = 5f;
 	private Vector3 v3StartPos;
@@ -101,7 +88,6 @@ public class DraggableObject : MonoBehaviour
 		if(draggable)
 		{
 			// Move object around screen
-			Screen.lockCursor = false;
 			dragging = true;
 			
 			// screenPoint.z is the distance in front of the camera (the interaction plane)
@@ -109,11 +95,12 @@ public class DraggableObject : MonoBehaviour
 			
 			// Translate the mouse's position to a point in the world
 			Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
-			rigidbody.MovePosition(curPosition);	// Use rigidbody to collide properly with other objects
+			//rigidbody.MovePosition(curPosition);	// Use rigidbody to collide properly with other objects
+			parent.transform.position = curPosition;
 		}
 	}
-
-
+	
+	
 	void Rotate()
 	{
 		// Holding right click and scrolling rotates object up and down
@@ -124,21 +111,8 @@ public class DraggableObject : MonoBehaviour
 			
 			if(rotationAmount != 0)
 			{
-				transform.Rotate(scrollDirection2 * rotationAmount);
+				parent.transform.Rotate(scrollDirection2 * rotationAmount);
 			}
-			
-			//Screen.lockCursor = true;
-			/*
-				// Record how much the mouse position has changed since the start of the rotation
-				v3ChangedPos.x += Input.GetAxis("Mouse X");
-				v3ChangedPos.y += Input.GetAxis("Mouse Y");
-
-				// Rotate object left and right
-				transform.localEulerAngles = v3StartRot + Vector3.up * (v3StartPos - v3ChangedPos).x * horizontalRotationalFactor;
-
-				// USe the mouse's change in Y position to rotate the object UP
-				transform.Rotate (Vector3.left * v3ChangedPos.y * verticalRotationalFactor);
-				*/
 		}
 		// Rotate object left and right
 		else
@@ -148,17 +122,8 @@ public class DraggableObject : MonoBehaviour
 			
 			if(rotationAmount != 0)
 			{
-				transform.Rotate (scrollDirection * rotationAmount);
+				parent.transform.Rotate (scrollDirection * rotationAmount);
 			}
-		}
-	}
-	
-	
-	void Update()
-	{
-		if(dragging && rotatable)
-		{
-
 		}
 	}
 }
