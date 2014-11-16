442ee262b278a8a1f6141983bb9d453abe3ddec4
Dustin Walker <dnelson14@gmail.com>
2014-11-16 15:20:56 -0600

    super basic menu text

---
+++ PistolsAtDawn/Assets/Scripts/TitleScreen/TextHighlight.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextHighlight : MonoBehaviour {
+
+	public Color highlightColor = Color.red;
+	public Color defaultColor = Color.white;
+
+	void OnMouseEnter() {
+		renderer.material.color = Color.red;
+	}
+
+	void OnMouseExit() {
+		renderer.material.color = Color.white;
+	}
+}
