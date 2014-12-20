9bcb2c2cf2afd16e754b82dc3efa0ea2bea6f079
Dustin Walker <dnelson14@gmail.com>
2014-12-20 16:24:57 -0600

    auto scrolling

---
+++ PistolsAtDawn/Assets/AutoScrollText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class AutoScrollText : MonoBehaviour {
+
+	public Text contentText;
+	public float scrollSpeed = 0.1f;
+
+	void OnGUI(){
+		Vector3 t = contentText.rectTransform.position;
+		t.y += scrollSpeed;
+		contentText.rectTransform.position = t;
+	}
+}
