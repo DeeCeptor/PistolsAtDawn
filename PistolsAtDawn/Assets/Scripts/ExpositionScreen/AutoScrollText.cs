bbadf038fb8aebee2581a6bbdccf3a62b31300f0
Dustin Walker <dnelson14@gmail.com>
2014-12-28 21:38:14 -0600

    stop scrolling when the text has been shown

--- PistolsAtDawn/Assets/Scripts/ExpositionScreen/AutoScrollText.cs
+++ PistolsAtDawn/Assets/Scripts/ExpositionScreen/AutoScrollText.cs
@@ -6,10 +6,25 @@ public class AutoScrollText : MonoBehaviour {
 
 	public Text contentText;
 	public float scrollSpeed = 0.1f;
+	public RectTransform scrollRectTransform;
+	private float endScrollYPos;
+
+	void Start(){
+		endScrollYPos = scrollRectTransform.rect.height - scrollRectTransform.offsetMax.y;
+	}
 
 	void OnGUI(){
-		Vector3 pos = contentText.rectTransform.position;
-		pos.y += scrollSpeed;
-		contentText.rectTransform.position = pos;
+		if (!hasFinishedScrolling()) {
+			Vector3 pos = contentText.rectTransform.position;
+			pos.y += scrollSpeed;
+			contentText.rectTransform.position = pos;
+		}
+	}
+
+	bool hasFinishedScrolling(){
+		if (contentText.rectTransform.position.y > endScrollYPos) {
+			return true;
+		}
+		return false;
 	}
 }
